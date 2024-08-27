bc250b8ab65f3768e71c3adede64650c7e9f598f
SergioDiorov <sergiodiorov.sifex@gmail.com>
2024-08-27 18:16:31 +0300

    feat/add hash router for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import GameScreen from './components/GameScreen/GameScreen';
 import ProdutScreen from './components/ProductScreen/ProdutScreen';
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<GameScreen />} />
         <Route path='/products' element={<ProdutScreen />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
